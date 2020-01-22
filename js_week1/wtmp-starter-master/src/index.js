d158fbc0bc074b03d748e43f75f5b235941c2383
wazzazzaa <tomi.eranne@gmail.com>
2020-01-22 15:15:34 +0200

    task4

--- js_week1/wtmp-starter-master/src/index.js
+++ js_week1/wtmp-starter-master/src/index.js
@@ -1,6 +1,6 @@
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
+/*const coursesEn = ["Hamburger, cream sauce and poiled potates",
   "Goan style fish curry and whole grain rice",
-  "Vegan Chili sin carne and whole grain rice",
+ "Vegan Chili sin carne and whole grain rice",
   "Broccoli puree soup, side salad with two napas",
   "Lunch baguette with BBQ-turkey filling",
   "Cheese / Chicken / Vege / Halloum burger and french fries"];
@@ -10,20 +10,25 @@ const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä peruna
   "vegaani Chili sin carne ja täysjyväriisi",
   "Parsakeittoa,lisäkesalaatti kahdella napaksella",
   "Lunch baguette with BBQ-turkey filling",
-  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];*/
+import lunchMenu from './assets/lunchMenu.json';
+const lunches = Object.values(lunchMenu.courses);
+const coursesFi = lunches.map(a => a.title_fi);
+const coursesEn = lunches.map(a => a.title_en);
+console.log('lunch menu array', coursesFi);
 
-var eLen = coursesEn.length;
-var etext= "<ul>";
-for (i = 0; i < eLen; i++) {
+let eLen = coursesEn.length;
+let etext= "<ul>";
+for (let i = 0; i < eLen; i++) {
   etext += "<li>" + coursesEn[i] + "</li>";
 }
 etext += "</ul>";
 
 document.getElementById("en").innerHTML = etext;
 
-var fLen = coursesFi.length;
-var ftext= "<ul>";
-for (i = 0; i < fLen; i++) {
+let fLen = coursesFi.length;
+let ftext= "<ul>";
+for (let i = 0; i < fLen; i++) {
   ftext += "<li>" + coursesFi[i] + "</li>";
 }
 ftext += "</ul>";
@@ -31,10 +36,10 @@ ftext += "</ul>";
 document.getElementById("fi").innerHTML = ftext;
 
 top.visible_id = 'fi';
-toggle_visibility=()=> {
-
-  var fi_e = document.getElementById('fi');
-  var en_e = document.getElementById('en');
+document.getElementById('btn1').addEventListener('click', toggle_visibility, true);
+function toggle_visibility() {
+  const fi_e = document.getElementById('fi');
+  const en_e = document.getElementById('en');
   if (top.visible_id == 'fi') {
     fi_e.style.display = 'none';
     en_e.style.display = 'block';
@@ -44,22 +49,23 @@ toggle_visibility=()=> {
     en_e.style.display = 'none';
     top.visible_id = 'fi';
   }
-};
-sortFunction=()=>{
+}
+document.getElementById('btn2').addEventListener('click', sortFunction, true);
+function sortFunction(){
   coursesFi.sort();
 
-  var fLen = coursesFi.length;
-  var ftext= "<ul>";
-  for (i = 0; i < fLen; i++) {
+  let fLen = coursesFi.length;
+  let ftext= "<ul>";
+  for (let i = 0; i < fLen; i++) {
     ftext += "<li>" + coursesFi[i] + "</li>";
   }
   ftext += "</ul>";
 
   document.getElementById("fi").innerHTML = ftext;
-};
+}
+document.getElementById('btn3').addEventListener('click',randomFunction, true);
 const random = document.querySelector('.random');
 let randomNumber = Math.floor(Math.random() * coursesFi.length);
-randomFunction=()=>{
+export function randomFunction(){
   random.textContent = coursesFi[randomNumber] ;
-};
-
+}
